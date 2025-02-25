4ab52579c09e361e8799eba62f1ea9ead694e5b0
Guillecasaus <guille.casaus@gmail.com>
2025-02-25 11:12:36 +0100

    Add express-validator for track validation and implement validation middleware

--- Tema5/app.js
+++ Tema5/app.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use("/api", require("./routes")) //Lee routes/index.js por defecto
+app.use("/api", require("./routes"))
 app.use(express.static("storage"))
 
 const port = process.env.PORT || 3000
