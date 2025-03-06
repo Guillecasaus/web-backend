bf682bf85eadf16e805df1d57a7ff6d63279fa4c
Guillecasaus <guille.casaus@gmail.com>
2025-03-06 11:35:55 +0100

    Add role-based access control to track creation and update test data for user authentication

--- MVC/routes/tracks.js
+++ MVC/routes/tracks.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const { getItems, getItem, createItem, updateItem, deleteItem } = require("../controllers/tracks")
 const { validatorCreateItem, validatorGetItem } = require("../validators/tracks")
 const authMiddleware = require("../middleware/session")
+const checkRol = require("../middleware/rol")
 
 router.get("/", authMiddleware, getItems)
-router.post("/", authMiddleware, validatorCreateItem, createItem)
 router.get("/:id", validatorGetItem, getItem)
 router.put("/:id", validatorGetItem, updateItem)
 router.delete("/:id", validatorGetItem, deleteItem)
-
+router.post("/", authMiddleware, checkRol(["admin"]), validatorCreateItem, createItem)
 
 module.exports = router
