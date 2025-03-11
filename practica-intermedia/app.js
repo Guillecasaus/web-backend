358915720d91076e541cd69a4b5c1b59dceb2286
Guillecasaus <guille.casaus@gmail.com>
2025-03-11 12:19:55 +0100

    Initialize Express application with MongoDB connection and basic middleware setup

---
+++ practica-intermedia/app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config();
+const dbConnect = require('./config/mongo')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api", require("./routes"))
+app.use(express.static("storage"))
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log("Servidor escuchando en el puerto " + port)
+    dbConnect()
+})
