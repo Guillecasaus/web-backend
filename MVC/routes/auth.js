3cbf39e8b2d5e02c8023e1831ce9b5cd2f447da9
Guillecasaus <guille.casaus@gmail.com>
2025-03-25 11:54:38 +0100

    Add OpenAPI documentation for user registration endpoint

--- MVC/routes/auth.js
+++ MVC/routes/auth.js
@@ -5,6 +5,30 @@ const router = express.Router();
 const { validatorRegister, validatorLogin } = require("../validators/auth");
 const { registerCtrl, loginCtrl } = require("../controllers/auth");
 
+
+/**
+ * @openapi
+ * /api/auth/register:
+ *  post:
+ *      tags:
+ *      - User
+ *      summary: "User registter"
+ *      description: Register a new user
+ *      requestBody:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#/components/schemas/user"
+ *      responses:
+ *          '200':
+ *              description: Returns the inserted object
+ *          '401':
+ *              description: Validation error
+ *      security:
+ *          - bearerAuth: []
+ */
+
+
 // Ruta para registrar
 router.post("/register", validatorRegister, registerCtrl);
 
