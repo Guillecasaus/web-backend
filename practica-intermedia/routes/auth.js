e49ed141d202d73caf29f30f1db76e8b3232c1e4
Guillecasaus <guille.casaus@gmail.com>
2025-03-27 11:42:48 +0100

    Add verify reset token route to enhance password recovery process

--- practica-intermedia/routes/auth.js
+++ practica-intermedia/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { validatorRegister, validatorLogin, validatorEmailValidation } = require("../validators/auth");
-const { registerCtrl, loginCtrl, validateEmailCtrl, recoverPasswordCtrl, resetPasswordCtrl } = require("../controllers/auth");
+const { registerCtrl, loginCtrl, validateEmailCtrl, recoverPasswordCtrl, resetPasswordCtrl, verifyResetTokenCtrl } = require("../controllers/auth");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/register", validatorRegister, registerCtrl);
@@ -9,5 +9,6 @@ router.post("/login", validatorLogin, loginCtrl);
 router.put("/validate", authMiddleware, validatorEmailValidation, validateEmailCtrl);
 router.post("/recover-password", recoverPasswordCtrl);
 router.post("/reset-password", resetPasswordCtrl);
+router.get("/recover-password/verify", verifyResetTokenCtrl);
 
 module.exports = router;
